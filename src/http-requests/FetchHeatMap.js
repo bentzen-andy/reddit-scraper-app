beee2ed2a122c258e05865337e017f4497dbac0e
Andrew Bentzen <bentzen.an@gmail.com>
2021-11-20 18:43:13 -0500

    fixes issue with subreddits posting twice to the Firebase db

--- src/http-requests/FetchHeatMap.js
+++ src/http-requests/FetchHeatMap.js
@@ -8,35 +8,41 @@ const FetchHeatMap = (props) => {
 
   useEffect(() => {
     setIsLoading(true);
+    const clientTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    const clientLocale = Intl.DateTimeFormat().resolvedOptions().locale;
 
     fetch(
       `https://www.reddit.com/r/${props.subreddit}/top.json?t=month&limit=100`
     )
       .then((response) => response.json())
       .then((data) => {
-        let redditData = data.data.children.map((item) => ({
-          upvote_ratio: item.data.upvote_ratio,
-          // unix_time: new Date(item.data.created_utc * 1000),
-          day: new Date(
-            new Date(item.data.created_utc * 1000).toLocaleString("en-US", {
-              timeZone: "America/New_York",
-            })
-          ).getDay(),
-          date: new Date(item.data.created_utc * 1000)
-            .toLocaleString("en-GB", { timeZone: "America/New_York" })
-            .split(", ")[0],
-          time: new Date(item.data.created_utc * 1000)
-            .toLocaleString("en-GB", { timeZone: "America/New_York" })
-            .split(", ")[1],
-          hour: new Date(item.data.created_utc * 1000)
-            .toLocaleString("en-GB", { timeZone: "America/New_York" })
-            .split(", ")[1]
-            .split(":")[0],
-        }));
+        const redditData = data.data.children.map((item) => {
+          const postCreationDate = new Date(item.data.created_utc * 1000);
+
+          return {
+            subreddit: props.subreddit,
+            upvote_ratio: item.data.upvote_ratio,
+            unix_time: postCreationDate,
+            day: postCreationDate.getDay(),
+            date: postCreationDate
+              .toLocaleString(clientLocale, { timeZone: clientTimeZone })
+              .split(", ")[0],
+            time: postCreationDate
+              .toLocaleString(clientLocale, { timeZone: clientTimeZone })
+              .split(", ")[1],
+            hour: postCreationDate
+              .toLocaleString(clientLocale, { timeZone: clientTimeZone })
+              .split(", ")[1]
+              .split(":")[0],
+          };
+        });
 
         redditData.sort((a, b) => b.upvote_ratio - a.upvote_ratio);
         redditData.sort((a_1, b_1) => parseInt(a_1.hour) - parseInt(b_1.hour));
         redditData.sort((a_2, b_2) => parseInt(a_2.day) - parseInt(b_2.day));
+
+        console.log(redditData);
+
         if (redditData) setRedditSubmissions(redditData);
         else setRedditSubmissions(null);
       })
@@ -46,18 +52,23 @@ const FetchHeatMap = (props) => {
   // send the subreddit search to the sever so we can track popular searches
   useEffect(() => {
     if (redditSubmissions) {
+      console.log("POST:");
+      console.log(redditSubmissions[0].subreddit.toLowerCase());
       fetch(
         "https://reddit-scraper-app-default-rtdb.firebaseio.com/subreddits.json",
         {
           method: "POST",
-          body: JSON.stringify({ subreddit: props.subreddit, count: 1 }),
+          body: JSON.stringify({
+            subreddit: redditSubmissions[0].subreddit,
+            count: 1,
+          }),
           headers: {
             "Content-Type": "application/json",
           },
         }
       );
     }
-  }, [props.subreddit, redditSubmissions]);
+  }, [redditSubmissions]);
 
   if (redditSubmissions && redditSubmissions.length === 0)
     return <p>No data... </p>;
