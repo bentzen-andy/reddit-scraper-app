be8143a59ee338bff5ecd3c139b8702ebcb50129
Andrew Bentzen <bentzen.an@gmail.com>
2021-11-22 13:08:24 -0500

    adds the following features: feature-5, feature-6

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -1,8 +1,9 @@
 import React from "react";
+import styles from "./Instructions.module.css";
 
 const Instructions = () => {
   return (
-    <div>
+    <div className={styles["instructions"]}>
       Search for your favorite subreddit below to find the best times to post.
     </div>
   );
