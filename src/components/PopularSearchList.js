03c12a4c537eeff750bbf28a333799deec386e2a
Andrew Bentzen <bentzen.an@gmail.com>
2021-11-22 21:55:25 -0500

    adds feature-22, feature-23, and feature-24: adds styles to popular search list

--- src/components/PopularSearchList.js
+++ src/components/PopularSearchList.js
@@ -1,17 +1,22 @@
 import React from "react";
+import styles from "./PopularSearchList.module.css";
 
 const PopularSearchList = (props) => {
-  if (!props.data) return <p>no data...</p>;
-  if (props.data.length === 0) return <p>no data...</p>;
-
   return (
-    <React.Fragment>
+    <div className={styles["popular-search-list"]}>
+      <div className={styles["popular-search-list__heading"]}>
+        Popular Searches
+      </div>
+      {(!props.data || props.data.length === 0) && <p>no data...</p>}
       {props.data.map((item) => (
-        <p key={item.subreddit}>
-          {item.subreddit}.....{item.count}
-        </p>
+        <div
+          className={styles["popular-search-list__subreddit"]}
+          key={item.subreddit}
+        >
+          {item.subreddit}
+        </div>
       ))}
-    </React.Fragment>
+    </div>
   );
 };
 
