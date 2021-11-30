605ab57e53a9e19a6f8952d287f74d4bdb0281dd
Andrew Bentzen <bentzen.an@gmail.com>
2021-11-30 13:56:54 -0500

    refactors with a helper function for readability

--- src/components/heatmap/GridSquare.js
+++ src/components/heatmap/GridSquare.js
@@ -2,11 +2,18 @@ import React from "react";
 import styles from "./GridSquare.module.css";
 
 const GridSquare = (props) => {
-  let postScore = props.numPosts / 1.0;
-  postScore = (postScore / props.maxDailyPosts) * 6;
-  postScore = postScore < 1 && postScore > 0 ? 1 : postScore;
-  postScore = Math.round(postScore);
-  const color = `color-${postScore}`;
+  const getColor = () => {
+    if (props.maxDailyPosts === 0) return "color-1";
+
+    let postScore = props.numPosts / 1.0;
+    postScore = (postScore / props.maxDailyPosts) * 6;
+    postScore = postScore < 1 && postScore > 0 ? 1 : postScore;
+    postScore = Math.round(postScore);
+    const color = `color-${postScore}`;
+    return color;
+  };
+
+  const color = getColor();
 
   return (
     <td className={`${styles["grid-square"]} ${styles[color]}`}>
