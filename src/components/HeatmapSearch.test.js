47f68690a093a782951581802b108af3abebf4bc
Andrew Bentzen <bentzen.an@gmail.com>
2021-12-02 10:35:53 -0500

    wrote tests for the searchbar, heatmap, and popular search components

---
+++ src/components/HeatmapSearch.test.js
@@ -0,0 +1,183 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import HeatmapSearch from "./HeatmapSearch";
+
+// ============================================================================================
+// Helper Functions
+// --------------------------------------------------------------------------------------------
+
+function typeSomethingIntoTheFormAndClickSubmit(arg, container) {
+  const inputElement = container.getElementsByClassName("searchbar__input--box")[0];
+  const buttonElement = container.getElementsByClassName("searchbar__btn")[0];
+  userEvent.click(inputElement);
+  userEvent.type(inputElement, arg);
+  userEvent.click(buttonElement);
+}
+
+// ============================================================================================
+describe("Tests for the heatmap under default starting conditions", () => {
+  it("renders a heatmap with a valid subreddit as input - test_id: HeatmapSearch_1", async () => {
+    // Arrange
+    const { container } = render(<HeatmapSearch onEnteredSubreddit={() => {}} />);
+    const savedFetch = window.fetch;
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => ({
+        data: {
+          children: [
+            { data: { created_utc: 1635006124 } },
+            { data: { created_utc: 1635116124 } },
+            { data: { created_utc: 1635226124 } },
+            { data: { created_utc: 1635336124 } },
+            { data: { created_utc: 1635446124 } },
+          ],
+        },
+      }),
+    });
+
+    // Act
+    typeSomethingIntoTheFormAndClickSubmit("videos", container);
+    // Assert
+    const tableElement = await screen.findByRole("table");
+    const tableDataElements = tableElement.querySelectorAll("td");
+    // confirm that it rendered a table
+    expect(tableElement).toBeInTheDocument();
+    // confirm that the table has the correct number of cells (<td> elements)
+    expect(tableDataElements).toHaveLength(175); // = 7 days * 24 hours + 7 day_labels
+
+    // reset fetch function
+    window.fetch = savedFetch;
+  });
+
+  it("renders a 'no data...' message if no data is available for the subreddit - test_id: HeatmapSearch_2", async () => {
+    // Arrange
+    const { container } = render(<HeatmapSearch onEnteredSubreddit={() => {}} />);
+    const savedFetch = window.fetch;
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => ({
+        data: {
+          children: [],
+        },
+      }),
+    });
+
+    // Act
+    typeSomethingIntoTheFormAndClickSubmit("videos", container);
+
+    // Assert
+    const textElement = await screen.findByText(/no data/i);
+    expect(textElement).toBeInTheDocument();
+
+    // reset fetch function
+    window.fetch = savedFetch;
+  });
+});
+
+// --------------------------------------------------------------------------------------------
+
+// ============================================================================================
+describe("Tests for the searchbar", () => {
+  it("disallows the search if the input is just whitespace - test_id: HeatmapSearch_10", async () => {
+    // Arrange
+    const { container } = render(<HeatmapSearch onEnteredSubreddit={() => {}} />);
+
+    // Act
+    typeSomethingIntoTheFormAndClickSubmit("  ", container);
+
+    // Assert
+    const buttonElement = container.getElementsByClassName("searchbar__btn")[0];
+    expect(buttonElement.disabled).toBe(true);
+  });
+
+  it("gives validation text if the user clicks the searchbar, types an invalid search, and then clicks away from the searchbar - test_id: HeatmapSearch_11", async () => {
+    // Arrange
+    const { container } = render(<HeatmapSearch onEnteredSubreddit={() => {}} />);
+
+    // Act
+    const inputElement = container.getElementsByClassName("searchbar__input--box")[0];
+    userEvent.click(inputElement);
+    userEvent.type(inputElement, "   ");
+    userEvent.tab();
+
+    // Assert
+    const textElement = screen.getByText(/Please enter a valid subreddit/i);
+    expect(textElement).toBeInTheDocument();
+  });
+
+  it("clears validation text as soon as any non-whitespace character is typed into the searchbar - test_id: HeatmapSearch_12", async () => {
+    // Arrange
+    const { container } = render(<HeatmapSearch onEnteredSubreddit={() => {}} />);
+
+    // Act #1
+    const inputElement = container.getElementsByClassName("searchbar__input--box")[0];
+    userEvent.click(inputElement);
+    userEvent.type(inputElement, "   ");
+    userEvent.tab();
+
+    // Assert #1
+    const textElement = screen.getByText(/Please enter a valid subreddit/i);
+    expect(textElement).toBeInTheDocument();
+
+    // Act #2
+    userEvent.click(inputElement);
+    userEvent.type(inputElement, "x");
+
+    // Assert #2
+    expect(textElement).not.toBeInTheDocument();
+  });
+
+  it("renders a an error message if given an invalid subreddit - test_id: HeatmapSearch_13", async () => {
+    // Arrange
+    const { container } = render(<HeatmapSearch onEnteredSubreddit={() => {}} />);
+
+    // Act
+    typeSomethingIntoTheFormAndClickSubmit("uiofjewioewjklf", container);
+
+    // Assert
+    const textElement = await screen.findByText(/Error: No data available for subreddit/i);
+    expect(textElement).toBeInTheDocument();
+  });
+
+  it("renders an error message if the subreddit is exclusively dedicated to explicit material (NSFW) - test_id: HeatmapSearch_14", async () => {
+    // Arrange
+    const { container } = render(<HeatmapSearch onEnteredSubreddit={() => {}} />);
+
+    // Act
+    typeSomethingIntoTheFormAndClickSubmit("crimescene", container); // crimeScene shows graphic depictions of crime and is labeled a NSFW subreddit
+
+    // Assert
+    const textElement = await screen.findByText(
+      /Explicit content not allowed. Please try a different subreddit/i
+    );
+    expect(textElement).toBeInTheDocument();
+  });
+
+  it("stores valid searches to DB to track popular searches - test_id: HeatmapSearch_15", async () => {
+    // I don't know how to test for this.
+    // I don't think I want to test this anyway because it seems like this test is just a test
+    // of the fetch function (which is implemented by the browser, and therefore not mine to test.)
+
+    // test stub
+    expect(true).toBe(true);
+  });
+
+  it("does NOT store invalid searches to DB for popular searches - test_id: HeatmapSearch_16", async () => {
+    // I don't know how to test for this.
+    // I don't think I want to test this anyway because it seems like this test is just a test
+    // of the fetch function (which is implemented by the browser, and therefore not mine to test.)
+
+    // test stub
+    expect(true).toBe(true);
+  });
+
+  it("does NOT store (to the popular search DB) valid searches for subreddits that are exclusively dedicated to explicit material (NSFW) - test_id: HeatmapSearch_17", async () => {
+    // I don't know how to test for this.
+    // I don't think I want to test this anyway because it seems like this test is just a test
+    // of the fetch function (which is implemented by the browser, and therefore not mine to test.)
+
+    // test stub
+    expect(true).toBe(true);
+  });
+});
+// --------------------------------------------------------------------------------------------
---
+++ src/http-requests/FetchPopularSearches.test.js
@@ -0,0 +1,270 @@
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+import FetchPopularSearches from "./FetchPopularSearches";
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+// ============================================================================================
+describe("Tests for the popular search list", () => {
+  it("renders a title heading: 'Popular Searches' - test_id: FetchPopularSearches_1", async () => {
+    // Arrange
+    let mockResponse = {
+      "-MptFQItqiR92t93CxST": { subreddit: "askreddit", count: 1 },
+      "-MptFQItqiR92t9S4xST": { subreddit: "askreddit", count: 1 },
+      "-MptFQItqiR3yt9SCxST": { subreddit: "pics", count: 1 },
+      "-MptFQItqiRy33t9CxST": { subreddit: "pics", count: 1 },
+      "-MptFQItqiRjef84CxST": { subreddit: "pics", count: 1 },
+      "-Mp8gQItqiRjef84CxST": { subreddit: "pics", count: 1 },
+      "-MptFQItqiR92t92jxST": { subreddit: "askreddit", count: 1 },
+      "-MptFe6tqiR92t9932jT": { subreddit: "maybemaybemaybe", count: 1 },
+      "-MUHUe6tqiR92t9932jT": { subreddit: "maybemaybemaybe", count: 1 },
+      "-Mp8fjhtqiR92t92jxST": { subreddit: "cfb", count: 1 },
+      "-Mp8fjhtqiR92t9j3xST": { subreddit: "cfb", count: 1 },
+      "-MptFe6t03rjJGTt92ST": { subreddit: "askscience", count: 1 },
+      "-M88Fe6t03rjJGTt92ST": { subreddit: "askscience", count: 1 },
+      "-MptFe6tqiR98JGmh3ST": { subreddit: "videos", count: 1 },
+    };
+
+    jest.spyOn(global, "fetch").mockImplementation(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(mockResponse),
+      })
+    );
+
+    // Act
+    // Use the asynchronous version of act to apply resolved promises
+    await act(async () => {
+      render(<FetchPopularSearches subreddit={""} />, container);
+    });
+
+    // Assert
+    const textElement = container.querySelector(".popular-search-list__heading");
+    expect(textElement.textContent).toBe("Popular Searches");
+
+    // Tear-down
+    global.fetch.mockRestore();
+  });
+
+  it("renders a list of 5 popular searches from the past - test_id: FetchPopularSearches_2", async () => {
+    // Setup
+    let mockResponse = {
+      "-MptFQItqiR92t93CxST": { subreddit: "askreddit", count: 1 },
+      "-MptFQItqiR92t9S4xST": { subreddit: "askreddit", count: 1 },
+      "-MptFQItqiR3yt9SCxST": { subreddit: "pics", count: 1 },
+      "-MptFQItqiRy33t9CxST": { subreddit: "pics", count: 1 },
+      "-MptFQItqiRjef84CxST": { subreddit: "pics", count: 1 },
+      "-Mp8gQItqiRjef84CxST": { subreddit: "pics", count: 1 },
+      "-MptFQItqiR92t92jxST": { subreddit: "askreddit", count: 1 },
+      "-MptFe6tqiR92t9932jT": { subreddit: "maybemaybemaybe", count: 1 },
+      "-MUHUe6tqiR92t9932jT": { subreddit: "maybemaybemaybe", count: 1 },
+      "-Mp8fjhtqiR92t92jxST": { subreddit: "cfb", count: 1 },
+      "-Mp8fjhtqiR92t9j3xST": { subreddit: "cfb", count: 1 },
+      "-MptFe6t03rjJGTt92ST": { subreddit: "askscience", count: 1 },
+      "-M88Fe6t03rjJGTt92ST": { subreddit: "askscience", count: 1 },
+      "-MptFe6tqiR98JGmh3ST": { subreddit: "videos", count: 1 },
+    };
+
+    jest.spyOn(global, "fetch").mockImplementation(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(mockResponse),
+      })
+    );
+
+    // Act
+    // Use the asynchronous version of act to apply resolved promises
+    await act(async () => {
+      render(<FetchPopularSearches subreddit={""} />, container);
+    });
+
+    // Assert
+    const popularSearchListElements = container.querySelectorAll(
+      ".popular-search-list__subreddit"
+    );
+    expect(popularSearchListElements).toHaveLength(5);
+
+    // Tear-down
+    global.fetch.mockRestore();
+  });
+
+  it("renders a list of 4 popular searches from the past - test_id: FetchPopularSearches_2a", async () => {
+    // Setup
+    let mockResponse = {
+      "-MptFQItqiR92t93CxST": { subreddit: "askreddit", count: 1 },
+      "-MptFQItqiR92t9S4xST": { subreddit: "askreddit", count: 1 },
+      "-MptFQItqiR3yt9SCxST": { subreddit: "pics", count: 1 },
+      "-MptFQItqiRy33t9CxST": { subreddit: "pics", count: 1 },
+      "-MptFQItqiRjef84CxST": { subreddit: "pics", count: 1 },
+      "-Mp8gQItqiRjef84CxST": { subreddit: "pics", count: 1 },
+      "-MptFQItqiR92t92jxST": { subreddit: "askreddit", count: 1 },
+      "-MptFe6tqiR92t9932jT": { subreddit: "maybemaybemaybe", count: 1 },
+      "-MUHUe6tqiR92t9932jT": { subreddit: "maybemaybemaybe", count: 1 },
+      "-Mp8fjhtqiR92t92jxST": { subreddit: "cfb", count: 1 },
+      "-Mp8fjhtqiR92t9j3xST": { subreddit: "cfb", count: 1 },
+    };
+
+    jest.spyOn(global, "fetch").mockImplementation(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(mockResponse),
+      })
+    );
+
+    // Act
+    // Use the asynchronous version of act to apply resolved promises
+    await act(async () => {
+      render(<FetchPopularSearches subreddit={""} />, container);
+    });
+
+    // Assert
+    const popularSearchListElements = container.querySelectorAll(
+      ".popular-search-list__subreddit"
+    );
+    expect(popularSearchListElements).toHaveLength(4);
+
+    // Tear-down
+    global.fetch.mockRestore();
+  });
+
+  it("renders a list of 3 popular searches from the past - test_id: FetchPopularSearches_2b", async () => {
+    // Setup
+    let mockResponse = {
+      "-MptFQItqiR92t93CxST": { subreddit: "askreddit", count: 1 },
+      "-MptFQItqiR92t9S4xST": { subreddit: "askreddit", count: 1 },
+      "-MptFQItqiR3yt9SCxST": { subreddit: "pics", count: 1 },
+      "-MptFQItqiRy33t9CxST": { subreddit: "pics", count: 1 },
+      "-MptFQItqiRjef84CxST": { subreddit: "pics", count: 1 },
+      "-Mp8gQItqiRjef84CxST": { subreddit: "pics", count: 1 },
+      "-MptFQItqiR92t92jxST": { subreddit: "askreddit", count: 1 },
+      "-MptFe6tqiR92t9932jT": { subreddit: "maybemaybemaybe", count: 1 },
+      "-MUHUe6tqiR92t9932jT": { subreddit: "maybemaybemaybe", count: 1 },
+    };
+
+    jest.spyOn(global, "fetch").mockImplementation(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(mockResponse),
+      })
+    );
+
+    // Act
+    // Use the asynchronous version of act to apply resolved promises
+    await act(async () => {
+      render(<FetchPopularSearches subreddit={""} />, container);
+    });
+
+    // Assert
+    const popularSearchListElements = container.querySelectorAll(
+      ".popular-search-list__subreddit"
+    );
+    expect(popularSearchListElements).toHaveLength(3);
+
+    // Tear-down
+    global.fetch.mockRestore();
+  });
+
+  it("renders a list of 0 popular searches from the past - test_id: FetchPopularSearches_2c", async () => {
+    // Setup
+    let mockResponse = {};
+
+    jest.spyOn(global, "fetch").mockImplementation(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(mockResponse),
+      })
+    );
+
+    // Act
+    // Use the asynchronous version of act to apply resolved promises
+    await act(async () => {
+      render(<FetchPopularSearches subreddit={""} />, container);
+    });
+
+    // Assert
+    const popularSearchListElements = container.querySelectorAll(
+      ".popular-search-list__subreddit"
+    );
+    expect(popularSearchListElements).toHaveLength(0);
+
+    // Tear-down
+    global.fetch.mockRestore();
+  });
+
+  it("renders a messages that says 'no data...' if no popular search data is available - test_id: FetchPopularSearches_3", async () => {
+    // Setup
+    let mockResponse = {};
+
+    jest.spyOn(global, "fetch").mockImplementation(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(mockResponse),
+      })
+    );
+
+    // Act
+    // Use the asynchronous version of act to apply resolved promises
+    await act(async () => {
+      render(<FetchPopularSearches subreddit={""} />, container);
+    });
+
+    // Assert
+    const el = container.querySelector(".popular-search-list__no-data");
+    expect(el.textContent).toBe("no data...");
+
+    // Tear-down
+    global.fetch.mockRestore();
+  });
+
+  it("renders a list of popular searches in descending order by popularity - test_id: FetchPopularSearches_4", async () => {
+    // Setup
+    let mockResponse = {
+      "-MptFQItqiR92t93CxST": { subreddit: "askreddit", count: 1 }, // ct = 4
+      "-MptFQItqiR92t9S4xST": { subreddit: "askreddit", count: 1 },
+      "-MptFQItqiR3yt9SCxST": { subreddit: "pics", count: 1 }, // ct = 5
+      "-MptFQItqiRy33t9CxST": { subreddit: "pics", count: 1 },
+      "-MptFQItqi3jef84CxST": { subreddit: "pics", count: 1 },
+      "-MptFQItqiRjef84CxST": { subreddit: "pics", count: 1 },
+      "-Mp8gQItqiRjef84CxST": { subreddit: "pics", count: 1 },
+      "-MptFQItqiR92t92jxST": { subreddit: "askreddit", count: 1 },
+      "-MptFQetqiR92t92jxST": { subreddit: "askreddit", count: 1 },
+      "-MptFe6tqiR92t9932jT": { subreddit: "maybemaybemaybe", count: 1 }, // ct = 2
+      "-MUHUe6tqiR92t9932jT": { subreddit: "maybemaybemaybe", count: 1 },
+      "-Mp8fjhtqiR92t92jxST": { subreddit: "cfb", count: 1 }, // ct = 3
+      "-Mp8fjhtqi692t92jxST": { subreddit: "cfb", count: 1 },
+      "-Mp8fjhtqiR92t9j3xST": { subreddit: "cfb", count: 1 },
+      "-MptFe6t03rjJGTt92ST": { subreddit: "askscience", count: 1 }, // ct = 1
+    };
+
+    jest.spyOn(global, "fetch").mockImplementation(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(mockResponse),
+      })
+    );
+
+    // Act
+    // Use the asynchronous version of act to apply resolved promises
+    await act(async () => {
+      render(<FetchPopularSearches subreddit={""} />, container);
+    });
+
+    // Assert
+    const popularSearchListElements = container.querySelectorAll(
+      ".popular-search-list__subreddit"
+    );
+    expect(popularSearchListElements[0].textContent).toBe("pics");
+    expect(popularSearchListElements[1].textContent).toBe("askreddit");
+    expect(popularSearchListElements[2].textContent).toBe("cfb");
+    expect(popularSearchListElements[3].textContent).toBe("maybemaybemaybe");
+    expect(popularSearchListElements[4].textContent).toBe("askscience");
+
+    // Tear-down
+    global.fetch.mockRestore();
+  });
+});
